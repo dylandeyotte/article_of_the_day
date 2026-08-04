13b26caf5a3fabda92cd8d4db219dbf118611b35
dylandeyotte <dylan091deyotte@gmail.com>
2026-08-03 17:39:28 -0700

    wiki request

---
+++ go.mod
@@ -0,0 +1,3 @@
+module GitHub.com/dylandeyotte/article_of_the_day
+
+go 1.25.1
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type Article interface {
+	FullArticle | RandomArticle
+}
+
+type RandomArticle struct {
+	Batchcomplete string `json:"batchcomplete"`
+	Continue      struct {
+		Rncontinue string `json:"rncontinue"`
+		Continue   string `json:"continue"`
+	} `json:"continue"`
+	Query struct {
+		Random []struct {
+			ID    int    `json:"id"`
+			Ns    int    `json:"ns"`
+			Title string `json:"title"`
+		} `json:"random"`
+	} `json:"query"`
+}
+
+type FullArticle struct {
+	Query struct {
+		Pages map[string]Page `json:"pages"`
+	} `json:"query"`
+}
+
+type Page struct {
+	PageID  int    `json:"pageid"`
+	Ns      int    `json:"ns"`
+	Title   string `json:"title"`
+	Extract string `json:"extract"`
+}
+
+const randomArticleURL = "https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=1&rnminsize=5000&format=json"
+
+func wikiRequest[T Article](url string, article T) (T, error) {
+	var zero T
+	client := &http.Client{}
+
+	req, err := http.NewRequest(
+		"GET",
+		url,
+		nil,
+	)
+	if err != nil {
+		return zero, err
+	}
+
+	req.Header.Set("User-Agent", "ArticleOfTheDay/1.0 (dylan091deyotte@gmail.com)")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return zero, err
+	}
+
+	defer resp.Body.Close()
+
+	if err := json.NewDecoder(resp.Body).Decode(&article); err != nil {
+		return zero, err
+	}
+
+	return article, nil
+}
+
+func main() {
+	var RA RandomArticle
+	var FA FullArticle
+
+	random, err := wikiRequest(randomArticleURL, RA)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	title := strings.Split(random.Query.Random[0].Title, " ")
+	newTitle := strings.Join(title, "_")
+
+	url := fmt.Sprintf("https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=%v&explaintext=1&format=json", newTitle)
+
+	full, err := wikiRequest(url, FA)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var page Page
+	for _, p := range full.Query.Pages {
+		page = p
+		break
+	}
+
+	fmt.Println(page.Extract[:300])
+}
